0e0ce14002478b848e74080b378b20cae0eb6b22
rhcpist <lysak.posta@gmail.com>
2016-10-28 12:15:17 +0300

    Commit #4: 28.10.2016

--- js/main.js
+++ js/main.js
@@ -1,36 +1,35 @@
 var app = angular.module('App', []);
 app.controller('bauCtrl', function($scope) {
     $scope.Bau = {};
-    //$scope.Bau.radiusCount = "Jakob"; 
+
     $scope.Calc = function(count) {
-        //$scope.Bau = {};
-        console.log(count);
+
         var caclDate = [
         	{	
-        		weight: 4*count,
-        		summ: 9.15*count
+        		glueWeight: 4*count,
+        		glueSumm: 9.15*count
         	}
         ];
-        //console.log(puttyMix.weight + ' ' + puttyMix.summ);
+
         $scope.Bau.radiusCount = count;
-        if(!caclDate[0].weight) {
-    		$scope.Bau.summ = 0;
+        if(!caclDate[0].glueWeight) {
+    		$scope.Bau.glueSumm = 0;
     		console.log("calc");
     	}
     	else {
-    		$scope.Bau.weight = caclDate[0].weight;
-        	$scope.Bau.summ = caclDate[0].summ.toFixed(2);  
+    		$scope.Bau.glueWeight = caclDate[0].glueWeight;
+        	$scope.Bau.glueSumm = caclDate[0].glueSumm.toFixed(2);  
     	}   
     };
 
     $scope.editGlue = function(weight) {
     	if(!weight) {
-    		$scope.Bau.summ = '';
+    		$scope.Bau.glueSumm = '';
     		console.log("edit");
     	}
     	else
     	{
-    		$scope.Bau.summ = (9.15*weight).toFixed(2);
+    		$scope.Bau.glueSumm = (9.15*weight).toFixed(2);
     	}		
     }
 
