3beac30206107aeb313e58713363cb9ca93087b9
rhcpist <lysak.posta@gmail.com>
2016-11-02 17:44:52 +0200

    Commit: 17:44 02-11-2016

--- js/main.js
+++ js/main.js
@@ -1,3 +1,40 @@
+var BauSystem = {};
+
+/* set up XMLHttpRequest */
+var url = "prices.xlsx";
+var oReq = new XMLHttpRequest();
+oReq.open("GET", url, true);
+oReq.responseType = "arraybuffer";
+
+oReq.onload = function(e) {
+  var arraybuffer = oReq.response;
+
+  /* convert data to binary string */
+  var data = new Uint8Array(arraybuffer);
+  var arr = new Array();
+  for(var i = 0; i != data.length; ++i) arr[i] = String.fromCharCode(data[i]);
+  var bstr = arr.join("");
+
+  /* Call XLSX */
+  var workbook = XLSX.read(bstr, {type:"binary"});
+
+  /* DO SOMETHING WITH workbook HERE */
+  var sheet = workbook.SheetNames[0];
+  var worksheet = workbook.Sheets[sheet];
+  var colCount = worksheet['D4'].v;
+  var colPrice = worksheet['F4'].v.toFixed(2);
+  BauSystem.count = colCount;
+  BauSystem.price = colPrice; 
+
+}
+	console.log(BauSystem.count);
+	console.log(BauSystem.price);
+
+oReq.send();
+
+
+
+
 var app = angular.module('App', []);
 
 app.controller('bauCtrl', function($scope) {
